99c8d84568ff51dffc187192c8c37ac0de7acf40
antonio allen <modusoul@gmail.com>
2020-08-08 04:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays ()
 return 'Happy holidays!'
 }// write your code below!
 
-function happyHolidaysTo (name)
+function happyHolidaysTo (Antonio)
 {
-  
+  return `Happy holidays, ${name}!`
 }
